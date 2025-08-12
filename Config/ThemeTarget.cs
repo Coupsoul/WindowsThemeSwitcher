baf26955a0ce3e4b507eac35f4fd38c2ad7b6e59
Coupsoul <slaw.kapustin2000@gmail.com>
2025-08-12 22:13:50 +0500

    Initial commit: Set up project structure and theme enums

---
+++ Config/ThemeTarget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsThemeSwitcher.Config
+{
+    internal class ThemeTarget
+    {
+        public enum TargetOfTheme
+        {
+            App,
+            System,
+            Both
+        }
+    }
+}
