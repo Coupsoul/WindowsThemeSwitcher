4a73a504eda8b5d13dd84a4b479a7f7cecd7a2b2
Coupsoul <slaw.kapustin2000@gmail.com>
2025-08-15 20:53:28 +0500

    Feat: Implement self-contained theme logic based on time

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using WindowsThemeSwitcher;
+using static WindowsThemeSwitcher.Config.ThemeTarget;
+using static WindowsThemeSwitcher.Config.ThemeType;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        TypeOfTheme themeToSet;
+        if (DateTime.Now.Hour >= 8 && DateTime.Now.Hour < 22)
+            themeToSet = TypeOfTheme.Light;
+        else
+            themeToSet = TypeOfTheme.Dark;
+
+        TargetOfTheme targetToSet = TargetOfTheme.Apps;
+        if (args.Length > 0)
+        {
+            switch (args[0].ToLower())
+            {
+                case "system": targetToSet = TargetOfTheme.System; break;
+                case "both": targetToSet = TargetOfTheme.Both; break;
+            }
+        }
+
+        ThemeChanger.ChangeTheme(targetToSet, themeToSet);
+    }
+}
--- ThemeChanger.cs
+++ ThemeChanger.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Win32;
-using WindowsThemeSwitcher.Config;
+﻿using Microsoft.Win32;
+using static WindowsThemeSwitcher.Config.ThemeTarget;
+using static WindowsThemeSwitcher.Config.ThemeType;
 
 namespace WindowsThemeSwitcher
 {
     internal static class ThemeChanger
     {
-        internal static void ChangeTheme(ThemeTarget.TargetOfTheme themeTarget, ThemeType.TypeOfTheme themeType)
+        internal static void ChangeTheme(TargetOfTheme themeTarget, TypeOfTheme themeType)
         {
             string keyPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize";
 
@@ -22,11 +18,13 @@ namespace WindowsThemeSwitcher
                     {
                         switch (themeTarget)
                         {
-                            case ThemeTarget.TargetOfTheme.Apps: key.SetValue("AppsUseLightTheme", (int)themeType);
+                            case TargetOfTheme.Apps: 
+                                key.SetValue("AppsUseLightTheme", (int)themeType);
                                 break;
-                            case ThemeTarget.TargetOfTheme.System: key.SetValue("SystemUsesLightTheme", (int)themeType);
+                            case TargetOfTheme.System: 
+                                key.SetValue("SystemUsesLightTheme", (int)themeType);
                                 break;
-                            case ThemeTarget.TargetOfTheme.Both:
+                            case TargetOfTheme.Both:
                                 key.SetValue("AppsUseLightTheme", (int)themeType);
                                 key.SetValue("SystemUsesLightTheme", (int)themeType);
                                 break;
